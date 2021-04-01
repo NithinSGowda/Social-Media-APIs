808e743df83b7b4ef27cf3b8bc55ca15bb4e3bd6
Kokila K N <kokilakn13@gmail.com>
2021-04-01 21:13:37 +0530

    Add files via upload

---
+++ Reddit/reddit_search.py
@@ -0,0 +1,42 @@
+import praw
+import pandas as pd
+import datetime as dt
+from praw.models import MoreComments
+import sys
+
+Search=sys.argv[1]
+
+reddit = praw.Reddit(client_id='qReU5pXkg46LcA', client_secret='HmxBqKB7ua_rbNVW3_8BUAg3kvlE4Q', user_agent='media monitoring')
+subreddit = reddit.subreddit(Search)
+top_subreddit = subreddit.top(limit=1)
+topics_dict = { "title":[], \
+                "score":[], \
+                "id":[], "url":[],"comms_num": [], \
+                "created": [], \
+                "body":[],"comments":[]}
+
+for submission in top_subreddit:
+    comment = []
+    topics_dict["title"].append(submission.title)
+    topics_dict["score"].append(submission.score)
+    topics_dict["id"].append(submission.id)
+    topics_dict["url"].append(submission.url)
+    topics_dict["comms_num"].append(submission.num_comments)
+    topics_dict["created"].append(submission.created)
+    topics_dict["body"].append(submission.selftext)
+    submissions = reddit.submission(submission.id)
+    for top_level_comment in submissions.comments:
+        if isinstance(top_level_comment, MoreComments):
+            continue
+        comment.append(top_level_comment.body)
+#     print(comment)
+    topics_dict["comments"].append(comment)
+
+topics_data = pd.DataFrame(topics_dict)
+def get_date(created):
+    return dt.datetime.fromtimestamp(created)
+
+_timestamp = topics_data["created"].apply(get_date)
+topics_data = topics_data.assign(timestamp = _timestamp)
+
+topics_data.to_json(Search+".json")
